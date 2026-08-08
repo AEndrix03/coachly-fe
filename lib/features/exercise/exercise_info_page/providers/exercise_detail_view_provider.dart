c6d5e13508a22948d06cbc4decb5ab998b73c1f1
AEndrix <redegalli.andrea03@gmail.com>
2026-08-08 03:07:04 +0200

    refactor(exercise): make detail mock the default source

--- lib/features/exercise/exercise_info_page/providers/exercise_detail_view_provider.dart
+++ lib/features/exercise/exercise_info_page/providers/exercise_detail_view_provider.dart
@@ -1,25 +1,12 @@
 import 'package:coachly/features/exercise/exercise_info_page/data/repositories/exercise_detail_view_repository.dart';
 import 'package:coachly/features/exercise/exercise_info_page/data/services/exercise_detail_view_service.dart';
 import 'package:coachly/features/exercise/exercise_info_page/domain/exercise_detail_view_data.dart';
-import 'package:coachly/features/exercise/exercise_info_page/providers/exercise_info_provider/exercise_info_provider.dart';
 import 'package:coachly/features/user_settings/providers/settings_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-const useExerciseDetailMocks = bool.fromEnvironment(
-  'USE_EXERCISE_DETAIL_MOCKS',
-  defaultValue: true,
+final exerciseDetailViewServiceProvider = Provider<ExerciseDetailViewService>(
+  (_) => const MockExerciseDetailViewService(),
 );
-
-final exerciseDetailViewServiceProvider = Provider<ExerciseDetailViewService>((
-  ref,
-) {
-  if (useExerciseDetailMocks) {
-    return const MockExerciseDetailViewService();
-  }
-  return ApiExerciseDetailViewService(
-    ref.watch(exerciseInfoPageRepositoryProvider),
-  );
-});
 
 final exerciseDetailViewRepositoryProvider =
     Provider<ExerciseDetailViewRepository>((ref) {
